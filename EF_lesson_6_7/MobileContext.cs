f514d9a00263224f95d2d4095b0d2df8d3466fde
Alex <alexis1784@gmail.com>
2016-01-29 14:56:07 +0500

    working version

---
+++ EF_lesson_6_7/MobileContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_lesson_6_7
+{
+    public class MobileContext : DbContext
+    {
+        public MobileContext():base("EF_lesson_6_7")
+        { }
+
+        public DbSet<Phone> Phones { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // переопределение добавления
+            modelBuilder.Entity<Phone>()
+                .MapToStoredProcedures(b => b.Insert(sp => sp.HasName("InsertPhone")
+                    .Parameter(pm => pm.Name, "Model")
+                    .Parameter(pm => pm.Price, "Price")
+                    .Result(rs => rs.Id, "Id")));
+
+            // переопределение обновления
+            modelBuilder.Entity<Phone>()
+                .MapToStoredProcedures(b => b.Update(sp => sp.HasName("UpdatePhone")
+                    .Parameter(pm => pm.Name, "Model")
+                    .Parameter(pm => pm.Price, "Price")
+                    .Parameter(pm => pm.Id, "Id")));
+
+            // переопределение удаления
+            modelBuilder.Entity<Phone>()
+                .MapToStoredProcedures(b => b.Delete(sp => sp.HasName("DeletePhone")
+                    .Parameter(pm => pm.Id, "Id")));
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ EF_lesson_6_7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_lesson_6_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (MobileContext db = new MobileContext())
+            {
+                Phone phone = new Phone { Name = "iPhone 6S", Price = 680 };
+                db.Phones.Add(phone);
+                db.SaveChanges();
+                Console.WriteLine("Id - {0}, Price - {1}", phone.Id, phone.Price);
+            }
+            Console.ReadLine();
+        }
+    }
+}
